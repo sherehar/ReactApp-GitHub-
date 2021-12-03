af86493fd14fcd7aef5fbd37b6994d055142ef11
Shereen Haj Mahmoud <62863339+sherehar@users.noreply.github.com>
2021-12-03 17:55:46 +0100

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../App.css'
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <p>Copyright &copy; 2021, Shereen</p>
+        </div>
+    )
+}
